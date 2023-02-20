49609c1306be1acf61d8351a0edad94937857fcf
Caleb Cupul <caleb.cupul@alumnos.udg.mx>
2023-02-20 17:34:48 -0600

    feat: hold and isHeld functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
 import { useState } from 'react'
 import Die from './Die'
+import { nanoid } from 'nanoid'
 
 function App() {
+  const [dices, setDices] = useState(allNewDice())
+
+  function allNewDice() {
+    let diceArray = []
+
+    for (let i = 0; i < 10; i++) {
+      diceArray.push({
+        id: nanoid(),
+        value: Math.ceil(Math.random() * 6),
+        isHeld: true,
+        hold: holdDice
+      })
+    }
+    return diceArray
+  }
+
+  // function holdDice(id) {
+  //   setDices(prevDices => {
+  //     return prevDices.map(dice => {
+  //       return dice.id === id ? { ...dice, isHeld: !dice.isHeld } : dice
+  //     })
+  //   })
+  // }
+
+  function holdDice(id) {
+    setDices(prevDices => prevDices.map(dice => dice.id === id ? { ...dice, isHeld: !dice.isHeld } : dice))
+  }
+
+  const diceElements = dices.map((dice) => <Die key={dice.id} id={dice.id} value={dice.value} isHeld={dice.isHeld} hold={holdDice}></Die>)
+
+  function rollDice() {
+    setDices(allNewDice())
+  }
+
+
   return (
     <div className='p-10 bg-dark-blue h-screen grid align-middle justify-center'>
       <div className='bg-white-50 max-w-md py-12 px-12 rounded-xl my-auto'>
@@ -10,19 +46,11 @@ function App() {
           <p className='mt-1'>Roll until all dice are the same. Click each dice to freeze it at its current value between rolls.</p>
         </div>
         <div className='mt-8 gap-6 text-xl font-bold self-center grid grid-cols-5 items-center place-items-center'>
-          <Die value={1}></Die>
-          <Die value={1}></Die>
-          <Die value={1}></Die>
-          <Die value={1}></Die>
-          <Die value={1}></Die>
-          <Die value={1}></Die>
-          <Die value={1}></Die>
-          <Die value={1}></Die>
-          <Die value={1}></Die>
-          <Die value={1}></Die>
+          {diceElements}
+
         </div>
         <div className='flex justify-center'>
-          <button className=' text-white font-inter tracking-wider bg-dark-purple px-8 py-2 rounded-md mt-6 '>Roll</button>
+          <button onClick={rollDice} className=' text-white font-inter tracking-wider bg-dark-purple px-8 py-2 rounded-md mt-6 '>Roll</button>
         </div>
       </div>
     </div>
